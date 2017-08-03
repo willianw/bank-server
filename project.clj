3d608a1b0aed1f03e5cb356db6e296b7e934902d
Willian Werner <willianwernerangelodacosta@gmail.com>
2017-08-03 12:38:54 -0700

    Resolução de conflitos de dependências

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ [org.clojure/clojure "1.8.0"]
-                  [org.clojure/clojure "1.2.0"]
+                  [org.clojure/data.json "0.2.6"]
                   [org.clojure/clojure-contrib "1.2.0"]
-                  [ring/ring-core "0.2.5"]
-                  [ring/ring-devel "0.2.5"]
                   [ring/ring-jetty-adapter "0.2.5"]
+                  [ring/ring-devel "0.2.5"]
+                  [ring/ring-core "0.2.5"]
+                  [clj-time "0.14.0"]
                   [compojure "0.4.0"]
                   [hiccup "0.2.6"]
                 ]
